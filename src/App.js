dbd92b586fceb205272d8885c2953577e0d0d699
Ddbot <rakotoniaina.andry@gmail.com>
2021-02-12 21:15:55 +0100

    Installed Roboto Font

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <ol>
+            <ul>Install Material UI</ul>
+          </ol>
         </p>
         <a
           className="App-link"
