597caaf65b3c8bc1f5c0ce0bd4b22de5b12a3a35
Ddbot <rakotoniaina.andry@gmail.com>
2021-02-14 19:21:34 +0100

    Installed mui icons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import { AppBar, Container, List, ListItem, ListItemText } from '@material-ui/core';
-import logo from './logo.svg';
-// import './App.css';
+import {
+  AppBar,
+  Button,
+  Container,
+  Grid,  
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Menu,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import styled from 'styled-components';
+
+const AppContainer = styled(Container)`
+  height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+`;
+
+const InstallGrid = styled(List)`
+display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
 
 function App() {
-  return (<Container max-width>
-    <AppBar className="App App-header">
-      Menu
+  return (<AppContainer max-width>
+    <AppBar position='static'>
+      <Toolbar>
+    <IconButton edge="start" color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6">
+    Menu
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
     </AppBar>
-    <Container>
-      <List>
-        <ListItem><ListItemText>Install Material UI</ListItemText></ListItem>
-        <ListItem><ListItemText>Import AppBar and replace header from CRA</ListItemText></ListItem>
-        <ListItem><ListItemText>Import AppBar and replace header from CRA</ListItemText></ListItem>
-      </List>
-    </Container>
-  </Container>
+        <InstallGrid component="ol">
+          <ListItem><ListItemText>Install Material UI</ListItemText></ListItem>
+          <ListItem><ListItemText>Import AppBar and replace header from CRA</ListItemText></ListItem>
+          <ListItem><ListItemText>Import Syled Component</ListItemText></ListItem>
+        </InstallGrid>
+  </AppContainer>
   );
 }
 
