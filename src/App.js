4ef1d39d24383aff13c3780ffb683d6aeb32e78c
Ddbot <rakotoniaina.andry@gmail.com>
2021-02-15 02:50:21 +0100

    Added theme switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
 	AppBar,
 	Button,
 	Container,
+	FormGroup,
+	FormControlLabel,
 	Grid,  
 	IconButton,
 	List,
@@ -10,18 +12,57 @@ import {
 	ListItemText,
 	Menu,
 	MenuItem,
+	Switch,
 	Toolbar,
 	Typography,
 } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Brightness2Icon from '@material-ui/icons/Brightness2';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import TranslateIcon from '@material-ui/icons/Translate';
 import styled from 'styled-components';
 
+const AntSwitch = withStyles((theme) => ({
+  root: {
+    width: 28,
+    height: 16,
+    padding: 0,
+    display: 'flex',
+  },
+  switchBase: {
+    padding: 2,
+    color: theme.palette.grey[500],
+    '&$checked': {
+      transform: 'translateX(12px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        opacity: 1,
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  },
+  thumb: {
+    width: 12,
+    height: 12,
+    boxShadow: 'none',
+  },
+  track: {
+    border: `1px solid ${theme.palette.grey[500]}`,
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.common.white,
+  },
+  checked: {},
+}))(Switch);
+
 const MenuBar = styled(Toolbar)`
-	button:nth-of-type(2) {
-		position: absolute;	
-		right: 0;
-	}
+	// button:nth-of-type(2) {
+	// 	position: absolute;	
+	// 	right: 0;
+	// }
 `;
 
 const AppContainer = styled(Container)`
@@ -38,9 +79,65 @@ display: flex;
   height: 100vh;
 `;
 
+const Div = styled(Typography)`
+	display: flex;
+	flex-flow: row nowrap;
+	align-items: center;
+	justify-content: flex-end;
+	scale: 0.75;
+
+	transform: translateX(30%);
+`;
+
+const ChangeLanguageIcon = styled(IconButton)`
+	scale: 0.75;
+	position: absolute;
+	right: 0;
+
+	font-size: 40px;
+`;
+
+const ChangeThemeSwitch = (props) => {
+	return <FormGroup>
+      <Div component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>		
+			<Grid item>
+				<IconButton
+					aria-label="sun icon"
+					aria-controls="menu-appbar"
+					aria-haspopup="false"
+					// onClick={handleMenu}
+					color="inherit"
+					style={ props.checked ? {opacity: 1 } : { opacity: 0 }}>
+					<Brightness7Icon />
+				</IconButton>
+			</Grid>	
+			<Grid item>
+					<AntSwitch checked={props.checked} name="checked" onChange={ props.changeFn} />
+			</Grid>
+			<Grid item>	
+				<IconButton
+					aria-label="moon icon"
+					aria-controls="menu-appbar"
+					aria-haspopup="false"
+					// onClick={handleMenu}
+					color="inherit"
+					style={ !props.checked ? {opacity: 1 } : { opacity: 0}}>					
+					<Brightness2Icon />
+				</IconButton>			
+			</Grid>
+        </Grid>
+      </Div>
+	</FormGroup>
+}
+
 
 function App() {
 	const [anchorEl, setAnchorEl] = useState(null);
+
+	const [state, setState] = useState({
+		checkedTheme: true,
+	});
 
 	function handleClick(e) {
 		e.persist();
@@ -51,6 +148,17 @@ function App() {
 		e.persist();
 		setAnchorEl(null);
 	}
+
+	const handleChange = (event) => {
+		// setState({ ...state, [event.target.name]: event.target.checkedTheme });
+		setState(prev => { 
+			return { ...state, checkedTheme: !prev.checkedTheme }
+		});
+	};
+	
+	useEffect(() => { 
+		console.log('Changement de state: ', state)
+	})
 
 	return (<>
     	<AppBar position='static'>
@@ -69,14 +177,15 @@ function App() {
 					<MenuItem onClick={handleClose}>Logout</MenuItem>
 				</Menu>
 				<Typography variant="h6">Andry Online</Typography>
-				<IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                // onClick={handleMenu}
-                color="inherit">
-					<AccountCircle />
-              	</IconButton>
+				<ChangeThemeSwitch checked={state.checkedTheme} changeFn={handleChange}/>
+				<ChangeLanguageIcon
+					aria-label="change language button"
+					aria-controls="menu-appbar"
+					aria-haspopup="true"
+					// onClick={handleChange}
+					color="inherit">
+					<TranslateIcon />
+				</ChangeLanguageIcon>				
   			</MenuBar>
     	</AppBar>
         <InstallGrid component="ol">
