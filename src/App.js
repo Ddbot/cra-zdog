5a544fbbbed56f15d368b55117adb4c58fa287a1
Dandibot <rakotoniaina.andry@gmail.com>
2021-03-29 22:41:01 +0200

    Added Z dimension in the 5 of HTMLIcon

--- src/App.js
+++ src/App.js
@@ -221,35 +221,21 @@ function App() {
 		setMove(payload);
 	};
 
-	// TO DO timeline
+	// ScrollTrigger
 	useEffect(() => {
+		const { body } = document;
 		let spans = Array.from(olRef.current.querySelectorAll('span.MuiTypography-displayBlock'));
-		let tl = gsap.timeline({
-			// yes, we can add it to an entire timeline!
-			scrollTrigger: {
-				trigger: spans[0],
-				pin: true,   // pin the trigger element while active
-				start: "top top", // when the top of the trigger hits the top of the viewport
-				end: "+=100", // end after scrolling 500px beyond the start
-				scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-				snap: {
-					snapTo: "labels", // snap to the closest label in the timeline
-					duration: {min: 0.2, max: 3}, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-					delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
-					ease: "power1.inOut" // the ease of the snap animation ("power3" by default)
-				}
-			}
-		});
-		
-		// add animations and labels to the timeline
-		tl.addLabel("start")
-			// .from(spans[0], {scale: 0.3, autoAlpha: 0})
-			.addLabel("color")
-			.from(spans[1], {scale: 0.3, autoAlpha: 0})
-			.addLabel("spin")
-			.to(spans[2], {rotation: 360, autoAlpha: 1})
-			.addLabel("end");
+		// if(spans){
+		// 	gsap.set(spans, { autoAlpha: 0});
+		// }
 
+		setTl(prev => {
+			prev.to(spans[0], {
+				scrollTrigger: spans[0],
+				autoAlpha: 1,
+				duration: 2 
+			});
+		});
 		tl.play();
 	}, []);
 
@@ -268,12 +254,11 @@ function App() {
 
 	useEffect(() => {
 		let svg = illuRef.current.querySelector('svg');
-		console.log('ILluref = ', illuRef.current)
 		gsap.set(svg, { attr: { viewBox: '0 0 100 145.5' }});
 	});
 
 	// useEffect(() => {
-	// 	let spans = olRef.current.querySelectorAll('span.MuiTypography-displayBlock');		
+	// 	if(Array.from(olRef.current.querySelectorAll('span.MuiTypography-displayBlock')).length === 3) console.log('fezefeafz');
 	// })
 
 	return (
